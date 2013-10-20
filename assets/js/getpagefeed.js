bf0364b9d854d4776df3be8caaf9f3fbf3ab911a
Amith <amith@Amith.(none)>
2013-10-21 00:16:58 +0530

    page design draft

---
+++ assets/js/getpagefeed.js
@@ -0,0 +1,16 @@
+var json_data;
+var feedhtml="";
+$(document).ready(function(){  
+	$("#page").niceScroll({cursorcolor:"#fff",horizrailenabled:false});
+	$.get("https://graph.facebook.com/432128853565990/feed?access_token=1421044221443245|PhTHykagFRe2EE1gIeMB3nROemg",function(feedjson){
+		for(i=0;i<feedjson.data.length;i++){
+			if(feedjson.data[i].type == "photo" && feedjson.data[i].message !=null){
+				feedhtml=feedhtml+"<div class='newsfeed clearfix'><div class='feedimg left'><img src='"+feedjson.data[i].picture+"' width='130px' height='97px'/></div><div class='left feedbody'><p class='feedpostedby'>"+feedjson.data[i].from.name+"<p><p class='feedmsg'>"+feedjson.data[i].message+"</p><p class='likes'><span class='likecount'>"+feedjson.data[i].likes.data.length+"&nbsp;</span>like this<span class='right'><a href='"+feedjson.data[i].link+"' target='_TOP'>View on Facebook</a></span></p></div></div>";
+			}
+			else{
+			}
+		}
+		json_data=feedjson;
+		$("#page").append(feedhtml);
+	});
+});
